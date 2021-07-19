e321dc37a733e283ea418a3a3664665028e7bd25
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-07-19 15:48:54 +0500

    start and help commands handling added

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ import tokens
 bot = telebot.TeleBot(tokens.TELEGRAM_BOT_TOKEN)
 
 # Logging settings
-logging.basicConfig(filename='logs.log',
-                    filemode='a',
-                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+logging.basicConfig(format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',
                     level=logging.DEBUG)
 log = logging.getLogger('logger')
@@ -36,7 +34,7 @@ def parse_message(message):
 
 
 @bot.message_handler(func=lambda message : '/pastebin' in message.text)
-def handle_command(message):
+def handle_pastebin_command(message):
     # Handling '/pastebin' command
 
     # User must reply to message with code
@@ -47,6 +45,31 @@ def handle_command(message):
     log.info(f'Got request from {name} for {lang} language')
 
     return bot.reply_to(message, pastebin.create_paste(name, code, lang), disable_web_page_preview=True)
+
+
+@bot.message_handler(commands=['start'])
+def handle_start_command(message):
+    # Handling '/start' command
+
+    log.info(f'{message.from_user.first_name} started bot')
+    reply = 'Добро пожаловать!\nВведите /help для получения справки по использованию бота.'
+
+    return bot.send_message(message.from_user.id, reply)
+
+
+@bot.message_handler(commands=['help'])
+def handle_help_command(message):
+    # Handling '/help' command
+
+    log.info(f'{message.from_user.first_name} reads help message')
+    reply = '''
+    Для того, чтобы бот загрузил ваш код на Pastebin для начала пришлите\
+ ему сообщение с кодом, затем ответьте на него командой /pastebin *язык*.\n
+Например:\n/pastebin c++ или /pastebin питон\n
+Бот обработает сообщение и пришлет ссылку на Pastebin с вашим кодом. \
+Каждая ссылка активна ровно 24 часа :)
+    '''
+    return bot.send_message(message.from_user.id, reply)
 
 
 if __name__ == '__main__':
